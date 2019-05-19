573e063719cc0344839b5fc98a9bf117472e0eba
Robson <rbsonvca@hotmail.com>
2019-05-19 00:01:40 -0300

    Adiciona validação de formulário refs #11

--- src/app/register-physio/register-physio.component.ts
+++ src/app/register-physio/register-physio.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ValidateBrService } from 'angular-validate-br';
 
 @Component({
   selector: 'app-register-physio',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPhysioComponent implements OnInit {
 
-  constructor() { }
+  registerForm: any;
+  //disableLogin: boolean = true;
+
+  constructor(private validateBrService: ValidateBrService) {}
 
   ngOnInit() {
+
+    this.loginForm = new FormGroup({
+    	'name': new FormControl('', [Validators.required]),
+      'cpf': new FormControl('', [Validators.required,
+                                    this.validateBrService.cpf,
+                                    Validators.minLength(11),
+                                    Validators.maxLength(11)]),
+      'phone' : new FormControl('', [Validators.required,
+      															Validators.minLength(8),
+      															Validators.maxLength(11)]),
+      'email' : new FormControl('', [Validators.required,
+     																Validators.email]),
+      'password': new FormControl('', [Validators.required,
+      																Validators.minLength(6)])
+    })
+
   }
 
 }
