80882194065feee7820711f93fc7da7ecc450835
Denilson das Mercês Amorim <dma_2012@hotmail.com>
2019-05-27 02:12:39 -0300

    Add login service

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -3,6 +3,8 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ValidateBrService } from 'angular-validate-br';
 
+import { AuthService } from '../auth.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -13,8 +15,10 @@ export class LoginComponent implements OnInit, AfterViewInit {
   loginForm: any;
   disableLogin: boolean = true;
 
-  constructor(public router: Router,
-              private validateBrService: ValidateBrService) {}
+  constructor(
+    public router: Router,
+    private validateBrService: ValidateBrService,
+    private authService: AuthService) {}
 
   ngOnInit() {
 
@@ -24,7 +28,7 @@ export class LoginComponent implements OnInit, AfterViewInit {
                                     Validators.minLength(11),
                                     Validators.maxLength(11)]),
       'password': new FormControl('', [Validators.required, Validators.minLength(6)])
-    })
+    });
 
   }
 
@@ -32,13 +36,25 @@ export class LoginComponent implements OnInit, AfterViewInit {
   }
 
   onLogin() {
-    localStorage.setItem('isLoggedin', 'true');
-    this.router.navigateByUrl('home');
+    const cpf = this.maskCpf(this.loginForm.get('cpf').value);
+    const password = this.loginForm.get('password').value;
+    this.authService.login(cpf, password).subscribe(hasLoggedIn => {
+      if(hasLoggedIn)
+        this.router.navigateByUrl('home');
+    });
   }
 
   onUpdateForm() {
     if(this.loginForm.valid) {
       this.disableLogin = false;
     }
+  }
+
+  private maskCpf(cpf: string): string {
+    const comp1 = cpf.substring(0, 3);
+    const comp2 = cpf.substring(3, 6);
+    const comp3 = cpf.substring(6, 9);
+    const comp4 = cpf.substring(9, 11);
+    return `${comp1}.${comp2}.${comp3}-${comp4}`;
   }
 }
