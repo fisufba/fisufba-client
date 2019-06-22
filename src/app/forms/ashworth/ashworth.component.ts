a87a1aae5bbf208c9ff4ac861653f7b62cc59692
Gabriele Luz <gabrieleluz@infojr.com.br>
2019-06-22 01:19:06 -0300

    dynamic ashworth form

--- src/app/forms/ashworth/ashworth.component.ts
+++ src/app/forms/ashworth/ashworth.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AshworthComponent implements OnInit {
 
-  constructor() { }
+  n:number = 1;
+  fields:any = Array.from({length: 1}, (x,i) => i);
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  updateFields() {
+    this.fields = Array.from({length: this.n}, (x,i) => i);
+  }
+
+  addElement() {
+    this.n++;
+    this.updateFields();
   }
 
 }
