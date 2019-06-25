3af539ba1e9bff206a5ac9160a450a7074ded276
Gabriele Luz <gabrieleluz@infojr.com.br>
2019-06-25 11:54:05 -0300

    adds OSWESTRY scale

--- src/app/forms/oswestry/oswestry.component.ts
+++ src/app/forms/oswestry/oswestry.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-oswestry',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OswestryComponent implements OnInit {
 
+  form: any;
+  score: number = 0;
+  width: number = 0;
+
   constructor() { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      'intensidadeDor': new FormControl(),
+      'cuidadosPessoais': new FormControl(),
+      'carga': new FormControl(),
+      'caminhar': new FormControl(),
+      'sentar': new FormControl(),
+      'posicao': new FormControl(),
+      'repouso': new FormControl(),
+      'atividadeSexual': new FormControl(),
+      'vidaSocial': new FormControl(),
+      'viagem': new FormControl(),
+    })
+  }
+
+  onUpdateForm() {
+    this.score = 0;
+    for( let value in this.form.value) {
+      this.score += Number(this.form.value[value]);
+    }
+    this.width = (100*this.score)/50;
   }
 
 }
