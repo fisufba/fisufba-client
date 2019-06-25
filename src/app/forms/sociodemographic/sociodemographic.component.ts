198b91c3b838540ba43fa08b136de5b050470136
Gabriele Luz <gabrieleluz@infojr.com.br>
2019-06-25 16:38:40 -0300

    sociodemographic form

--- src/app/forms/sociodemographic/sociodemographic.component.ts
+++ src/app/forms/sociodemographic/sociodemographic.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators  } from '@angular/forms';
 
 @Component({
   selector: 'app-sociodemographic',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SociodemographicComponent implements OnInit {
 
+  form: any;
+
   constructor() { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      'civilSatus': new FormControl(),
+      'habitationStatus': new FormControl(),
+      'education': new FormControl(),
+      'ocupationalStatus': new FormControl(),
+      'currentJob': new FormControl(),
+      'lastJob': new FormControl(),
+      'sick': new FormControl(),
+      'medicine': new FormControl(),
+    });
+
   }
 
 }
