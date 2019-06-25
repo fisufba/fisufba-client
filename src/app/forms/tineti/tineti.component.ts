b20e3ff44146f3592dcff71c4761e45ad89da9c6
Gabriele Luz <gabrieleluz@infojr.com.br>
2019-06-25 00:36:51 -0300

    adds balance score to TINETI form closes #45

--- src/app/forms/tineti/tineti.component.ts
+++ src/app/forms/tineti/tineti.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-tineti',
@@ -7,12 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TinetiComponent implements OnInit {
   score:number = 0;
+  form: any;
+  width: number = 0;
 
   constructor() { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+     'equilibrioSentado': new FormControl(),
+     'levanta': new FormControl(),
+     'tentativa': new FormControl(),
+     'equilibrioImediato': new FormControl(),
+     'equilibrio': new FormControl(),
+     'desequilibrio': new FormControl(),
+     'olhosFechados': new FormControl(),
+     'girar': new FormControl(),
+     'sentar': new FormControl(),
+    });
   }
 
   onUpdateForm() {
+      console.log(this.score)
+      this.score = 0;
+      for( let value in this.form.value) {
+          this.score += Number(this.form.value[value]);
+      }
+      this.width = (100*this.score)/16;
   }
 }
